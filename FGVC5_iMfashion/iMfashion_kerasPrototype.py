617cea2ad5ecf27c8fd8c34701e6fac8f7487d88
mrchou <imchou239@gmail.com>
2018-05-04 17:47:50 +0800

    kerasPrototype: add model_trainner -- can be imported to train models.

--- FGVC5_iMfashion/iMfashion_kerasPrototype.py
+++ FGVC5_iMfashion/iMfashion_kerasPrototype.py
@@ -7,64 +7,39 @@ define different model used in iMaterialist Challenge(Fashion):
 https://www.kaggle.com/c/imaterialist-challenge-fashion-2018
 
 """
-
-from keras.preprocessing import image
+import os
+import logging
 from keras.models import Model
 from keras.layers import Dense, GlobalAveragePooling2D
 from keras.applications.vgg16 import VGG16
 from keras.applications.inception_v3 import InceptionV3
 from keras.utils import multi_gpu_model
 from keras.callbacks import Callback
-from keras.callbacks import ModelCheckpoint
+from FGVC5_iMfashion.iMfashion_ImgBatchLoader import ImgBatchLoader
 
-import logging
-logging.basicConfig(format='%(asctime)s %(message)s',
-        datefmt='%m/%d/%Y %I:%M:%S %p',
-        filename='training.log', level=logging.INFO
-        )
-import matplotlib.pyplot as plt
-#from FGVC5_iMfashion.iMfashion_ImgBatchLoader import ImgBatchLoader
-from iMfashion_ImgBatchLoader import ImgBatchLoader
-
-def model_vgg16():
-    model = VGG16(weights='imagenet', include_top=False)
-    return model
-
-def model_inceptionV3():
+def model_IncepV3_iM():
     base_model = InceptionV3(weights='imagenet', include_top=False)
 
     # flatten vs avgPooling: https://github.com/keras-team/keras/issues/8470
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
-    # model.add(Dropout(0.5))
     x = Dense(1024, activation='relu')(x)
-    # model.add(Dropout(0.5))
     predictions = Dense(228, activation='sigmoid')(x)
 
     model = Model(inputs=base_model.input, outputs=predictions)
-
-    # don't changed the weight of base_model.
-    # for layer in base_model.layers:
-    #     layer.trainable = False
-    
     return model
-    
 
-def plot_history(fit_history):
-    # list all data in history
-    # print(fit_history.history.keys())
-    # summarize history for loss
-    # plt.plot(fit_history.history['loss'])
-    # plt.plot(fit_history.history['val_loss'])
-    plt.plot(fit_history.epoch_losses)
-    plt.plot(fit_history.epoch_val_losses)
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.savefig('lost.png')
+def model_VGG16():
+    base_model = VGG16(weights='imagenet', include_top=False)
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(1024, activation='relu')(x)
+    predictions = Dense(228, activation='sigmoid')(x)
 
-class train_history(Callback):
+    model = Model(inputs=base_model.input, outputs=predictions)
+    return model
+
+class _train_history(Callback):
     def on_train_begin(self, logs={}):
         self.batch_losses = []
         self.epoch_val_losses = []
@@ -75,75 +50,111 @@ class train_history(Callback):
         self.epoch_val_losses.append(logs.get('val_loss'))
         self.epoch_losses.append(logs.get('loss'))
 
+class model_trainner():
+
+    def __init__(self, model, model_name, train_path, train_label, vali_path, vali_label):
+        self.model = model
+        self.model_name = model_name
+        self.train_path = train_path
+        self.train_label = train_label
+        self.vali_path = vali_path
+        self.vali_label = vali_label
+
+    def log_config(self):
+        logging.basicConfig(format='%(asctime)s %(message)s',
+                            datefmt='%m/%d/%Y %I:%M:%S %p',
+                            filename='model_trainner.log', level=logging.INFO
+                            )
+
+    def fit(self, optimizer, loss, batch_size, epoch, multi_gpu=2, workers=16, queue=10, log=True):
+        assert batch_size >= 1 and batch_size%1==0, 'Batchsize must be natural numbers.'
+        assert epoch>=1 and epoch%1==0, 'Epoch must be natural numbers.'
+        if log:
+            self.log_config()
+
+        # create images generator
+        imgs_train = ImgBatchLoader(img_path=self.train_path, img_label=self.train_label)
+        imgs_vali = ImgBatchLoader(img_path=self.vali_path, img_label=self.vali_label)
+
+        # compile model with gpu setting
+        target = multi_gpu_model(self.model, gpus=multi_gpu) if multi_gpu else self.model
+        target.compile(optimizer=optimizer, loss=loss)
+        target.summary()
+
+        history = _train_history()
+        train_steps = len([i for i in os.listdir(self.train_path) if i.lower().endswith('.jpg')])/batch_size
+        vali_steps = len([i for i in os.listdir(self.vali_path) if i.lower().endswith('.jpg')])/batch_size
+        target.fit_generator(validation_steps=vali_steps,
+                            steps_per_epoch=train_steps, epochs=epoch,
+                            generator=imgs_train.generator(batch_size),
+                            validation_data=imgs_vali.generator(batch_size),
+                            use_multiprocessing=True,
+                            workers=workers,
+                            max_queue_size=queue,
+                            callbacks=[history]
+                            )
+
+        self.model.save(self.model_name + '.h5')
+        fw = open(self.model_name + '.info', 'w')
+        fw.writelines(['Filename: {0}'.format(self.model_name + '.h5'), \
+                       'Model Discription:', \
+                       'Base Model:',\
+                       'Top Model:',\
+                       'Optimizer: {0}'.format(optimizer),\
+                       'Loss function: {0}'.format(loss),\
+                       'Train Data: {0}'.format(self.train_path),\
+                       'Validation Data: {0}'.format(self.vali_path),\
+                       'Fit: batchsize={0}; epoch={1}'.format(batch_size, epoch), \
+                       'Scores: train loss=; vali loss=']
+                      )
+        fw.close()
+
+        if log:
+            logging.info(history.batch_losses)
+            logging.info(history.epoch_losses)
+            logging.info(history.epoch_val_losses)
+
+
+import matplotlib.pyplot as plt
 def main():
-    model = model_inceptionV3()
-    model.summary()
 
     train_path = '/rawdata/FGVC5_iMfashion/imgs_train/'
     train_label = '/archive/iMfashion/labels/labels_train.pickle'
     vali_path = '/rawdata/FGVC5_iMfashion/imgs_validation/'
     vali_label = '/archive/iMfashion/labels/labels_validation.pickle'
-    
-    train_loader = ImgBatchLoader(img_path=train_path, img_label=train_label)
-    vali_loader = ImgBatchLoader(img_path=vali_path, img_label=vali_label)
-    '''
-    for i in train_loader.generator(10, shuffle=True):
-        print(i[0].shape)
-        print(i[1].shape)
-        print('=====')
-    '''
-    # use 2 GPU
-    parallel_model = multi_gpu_model(model, gpus=2)
-    
-    # compile the model
-    
-    # parallel_moidel.fit(train_dataset, train_label, epochs=3, batch_size=300, 
-    #         validation_split=0.1, shuffle=True)
-    
-    parallel_model.compile(optimizer='rmsprop', loss='binary_crossentropy')
-    parallel_model.summary()
 
-    # checkpoint
-    '''
-    filepath="best_w.h5"
-    checkpoint = ModelCheckpoint(filepath, monitor='val_loss',
-            verbose=1, save_best_only=True, mode='min')
-    # callbacks_list = [checkpoint]
-    '''
-    
-    history = train_history()
-    parallel_model.fit_generator(generator=train_loader.generator(128),
-            validation_data=vali_loader.generator(128),
-            validation_steps=9900//128,
-            steps_per_epoch=1014547//128, epochs=5,
-            use_multiprocessing=True,
-            workers=16,
-            max_queue_size=10,
-            callbacks=[history]#, checkpoint]
-            )
-    '''
-    parallel_model.fit_generator(generator=vali_loader.generator(128),
-            validation_data=vali_loader.generator(128),
-            validation_steps=9900//128,
-            steps_per_epoch=9900//128, epochs=2,
-            use_multiprocessing=True,
-            workers=16,
-            max_queue_size=10,
-            callbacks=[history]#, checkpoint]
-            )
-    '''
-    # score = parallel_model.evaluate_generator(vali_loader.generator(128), 990/128, workers=16)
-    # scores = parallel_model.predict_generator(vali_loader.generator(128), 990//128, workers=16)
-    # print(score)
-    # check point will save the best model (val_loss lowest)
-    model.save('test_model_all_vali_0503_nofit.h5')
-    
-    logging.info(history.batch_losses)
-    logging.info(history.epoch_losses)
-    logging.info(history.epoch_val_losses)
-    
+    s = model_trainner(model=model_IncepV3_iM(),
+                       model_name='IncepV3_0504_iM2',
+                       train_path=train_path,
+                       train_label=train_label,
+                       vali_path=vali_path,
+                       vali_label=vali_label,
+                       )
+
+    s.fit(optimizer='rmsprop',
+          loss='binary_crossentropy',
+          batch_size=128,
+          epoch=5,
+          multi_gpu=2,
+          log=True
+          )
+
+    # summarize history for loss
+    def plot_history(fit_history):
+        # list all data in history
+        # print(fit_history.history.keys())
+        # plt.plot(fit_history.history['loss'])
+        # plt.plot(fit_history.history['val_loss'])
+        plt.plot(fit_history.epoch_losses)
+        plt.plot(fit_history.epoch_val_losses)
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.savefig('lost.png')
+
     # plot training process
-    # plot_history(history)
+    #plot_history(history)
 
 if __name__=='__main__':
     main()
